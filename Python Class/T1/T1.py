6d1d53d906d10e8c3101ab81974ab5478accea7b
Hadi Yazdani <85439533+MinisterHD@users.noreply.github.com>
2023-12-20 21:01:45 +0330

    Add files via upload

--- Python Class/T1/T1.py
+++ Python Class/T1/T1.py
@@ -1,16 +1,14 @@
-# With While Loop
+#With While Loop
 x = int(input("Enter a Number :"))
 i = 0
 while i >= 0:
     print(i)
     i = i - 1
-
 y = x
 while y >= 0:
     print(y)
     y = y - 1
-# # With For Loop
-
+#With For Loop
 for a in range(x):
     print(a)
     
