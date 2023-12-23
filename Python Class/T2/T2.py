3658121dfec0643f2e1d97ffd6704e7454034edc
Hadi Yazdani <85439533+MinisterHD@users.noreply.github.com>
2023-12-23 13:39:14 +0330

    Update T2.py

--- Python Class/T2/T2.py
+++ Python Class/T2/T2.py
@@ -1,7 +1,6 @@
 z = int(input("Enter the Number Of Students: "))
 for i in range(z):
     x = int(input("Enter the score: "))
-
     if x < 0 or x > 20:
         print("Not Valid")
     while x < 0 or x > 20:  
