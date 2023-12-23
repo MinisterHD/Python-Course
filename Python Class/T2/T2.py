d1753a2d2b119df3712341c1eb520c447aa947a2
Hadi Yazdani <85439533+MinisterHD@users.noreply.github.com>
2023-12-23 19:47:08 +0330

    Add files via upload

--- Python Class/T2/T2.py
+++ Python Class/T2/T2.py
@@ -1,12 +1,12 @@
 z = int(input("Enter the Number Of Students: "))
 for i in range(z):
     x = int(input("Enter the score: "))
-    if x < 0 or x > 20:
-        print("Not Valid")
+    
     while x < 0 or x > 20:  
+        print("Not Valid")
         x = int(input("Enter a Valid Score: "))
+   
+    if x >= 10:
+        print("Passed")
     else:
-        if x >= 10:
-            print("Passed")
-        else:
-            print("Failed")
+        print("Failed")
