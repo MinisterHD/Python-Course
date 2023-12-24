adbfb2023c79299edfe387775b02760584cc1349
Hadi Yazdani <85439533+MinisterHD@users.noreply.github.com>
2023-12-24 14:01:04 +0330

    Add files via upload

--- Python Class/T3/T3.py
+++ Python Class/T3/T3.py
@@ -6,9 +6,9 @@ PCPoints=0
 z=int(input("How Many Rounds You Want to Play ? "))
 for i in range(z):
     PlayerMove=input("Make your Move : ")
-    while PlayerMove not in ["Rock","rock","Paper","paper","Scissors","scissors"]:
-        print("Invalid Value")
-        PlayerMove=input("Make your Move Again : ")
+#    while PlayerMove not in ["Rock","rock","Paper","paper","Scissors","scissors"]:
+#        print("Invalid Value")
+#        PlayerMove=input("Make your Move Again : ")
     if (PlayerMove=="Rock") or (PlayerMove=="rock") :
         print("Draw")
     elif PlayerMove=="Paper" or (PlayerMove=="paper"):
@@ -17,9 +17,11 @@ for i in range(z):
     elif (PlayerMove=="Scissors") or (PlayerMove=="scissors" ):
         print("Pc Wins")
         PCPoints=PCPoints+1
+    else:
+        print("Invalid Value")
 if PlayerPoints>PCPoints:
     print(f"Player Wins the Game {PlayerPoints} vs {PCPoints}")
 elif PlayerPoints==PCPoints:
-    print(f"The Game is a Draw{PlayerPoints} vs {PCPoints}")
+    print(f"The Game is a Draw {PlayerPoints} vs {PCPoints}")
 else :
     print(f"PC Wins the Game {PCPoints} vs {PlayerPoints}")
